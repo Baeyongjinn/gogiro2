ddf0867a804f8afbede56868b657fe90ac355e35
배용진 <dydwls2270@naver.com>
2024-01-05 15:05:37 +0900

    test

---
+++ src/main/java/com/green/gogiro/GogiroApplication.java
@@ -0,0 +1,13 @@
+package com.green.gogiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GogiroApplication.class, args);
+    }
+
+}
