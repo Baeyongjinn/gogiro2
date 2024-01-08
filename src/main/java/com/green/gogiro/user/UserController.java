8e8531599d6a7a52c376da4aab97f214e4de8f66
dongki <ehdgusdl0816@gmail.com>
2024-01-08 16:05:36 +0900

    test2

--- src/main/java/com/green/gogiro/user/UserController.java
+++ src/main/java/com/green/gogiro/user/UserController.java
@@ -1,4 +1,13 @@
 package com.green.gogiro.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+
+    private final UserService service;
 }
---
+++ src/main/java/com/green/gogiro/user/UserMapper.java
@@ -0,0 +1,7 @@
+package com.green.gogiro.user;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+}
