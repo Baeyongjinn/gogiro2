dcc2633d4ba7c44b7c306020fee132302d34cd70
배용진 <dydwls2270@naver.com>
2024-01-08 16:59:53 +0900

    7교시

--- src/main/java/com/green/gogiro/user/UserService.java
+++ src/main/java/com/green/gogiro/user/UserService.java
@@ -1,12 +1,16 @@
 package com.green.gogiro.user;
 
 import com.green.gogiro.common.ResVo;
+import com.green.gogiro.errortest.CategoryNotFoundException;
 import com.green.gogiro.user.model.UserSigninDto;
 import com.green.gogiro.user.model.UserSignupDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
+
+import static com.green.gogiro.errortest.ErrorCode.SIGNIN_UID_ERROR;
+import static com.green.gogiro.errortest.ErrorCode.SIGNIN_UPW_ERROR;
 
 @Slf4j
 @Service
@@ -27,10 +31,10 @@ public class UserService {
     public ResVo signin(UserSigninDto dto){
         String check = mapper.signinUser(dto.getEmail());
         if(check == null){
-            return new ResVo(3);
+            throw new CategoryNotFoundException(SIGNIN_UID_ERROR);
         }
-        else if (!(BCrypt.checkpw(dto.getUpw(),check))) {
-            return new ResVo(2);
+        if (!(BCrypt.checkpw(dto.getUpw(),check))) {
+            throw new CategoryNotFoundException(SIGNIN_UPW_ERROR);
         }
         return new ResVo(1);
     }
