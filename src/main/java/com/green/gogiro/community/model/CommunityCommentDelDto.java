0eae78b67dcc07049020a20a4297d8cb8ccf66ad
배용진 <dydwls2270@naver.com>
2024-01-15 17:07:50 +0900

    2024-01-15 스키마 추가 2

--- src/main/java/com/green/gogiro/community/model/CommunityCommentDelDto.java
+++ src/main/java/com/green/gogiro/community/model/CommunityCommentDelDto.java
@@ -1,9 +1,12 @@
 package com.green.gogiro.community.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CommunityCommentDelDto {
+    @Schema(title = "유저pk")
     private int iuser;
+    @Schema(title = "댓글pk")
     private int icomment;
 }
--- src/main/java/com/green/gogiro/community/model/CommunityCommentInsDto.java
+++ src/main/java/com/green/gogiro/community/model/CommunityCommentInsDto.java
@@ -1,11 +1,15 @@
 package com.green.gogiro.community.model;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CommunityCommentInsDto {
+    @Schema(title = "커뮤니티pk")
     private int iboard;
+    @Schema(title = "유저pk")
     private int iuser;
+    @Schema(title = "댓글내용")
     private String contents;
 }
