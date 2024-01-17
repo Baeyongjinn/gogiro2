65797c1b96588f2b906f1bd26c84710dace6ff43
배용진 <dydwls2270@naver.com>
2024-01-17 17:43:18 +0900

    2024-01-17 추가 작업 11

--- src/main/java/com/green/gogiro/reservation/ReservationService.java
+++ src/main/java/com/green/gogiro/reservation/ReservationService.java
@@ -2,22 +2,40 @@ package com.green.gogiro.reservation;
 
 import static com.green.gogiro.common.Const.*;
 
+import com.green.gogiro.common.Const;
 import com.green.gogiro.common.ResVo;
+import com.green.gogiro.exception.AuthErrorCode;
+import com.green.gogiro.exception.RestApiException;
 import com.green.gogiro.reservation.model.*;
 import com.green.gogiro.security.AuthenticationFacade;
+import com.green.gogiro.shop.ShopMapper;
+import com.green.gogiro.shop.model.ShopEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.regex.Pattern;
 
 @RequiredArgsConstructor
 @Service
 public class ReservationService {
     private final ReservationMapper mapper;
     private final AuthenticationFacade authenticationFacade;
+    private final ShopMapper shopMapper;
 
 
     public ResVo postReservation(ReservationInsDto dto) {
+        ShopEntity entity = shopMapper.selShopEntity(dto.getIshop());
+        if(entity == null){
+            throw new RestApiException(AuthErrorCode.VALID_SHOP);
+        }if(entity.getIshop() != dto.getIshop()){
+            throw new RestApiException(AuthErrorCode.CHECK_SHOP);
+        }
+        if(dto.getDate().equals("0000-00-00 00:00:00") || Pattern.matches(REGEXP_PATTERN_SPACE_CHAR,dto.getDate())){
+            throw new RestApiException(AuthErrorCode.NOT_DATE);
+        }
         dto.setIuser(authenticationFacade.getLoginUserPk());
-        return new ResVo(mapper.insReservation(dto));
+        mapper.insReservation(dto);
+        return new ResVo(SUCCESS);
     }
 
     public ResVo postPickup(PickupInsDto dto) {
