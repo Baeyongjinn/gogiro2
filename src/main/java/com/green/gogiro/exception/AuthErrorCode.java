068ebad607d2935fc6949ef7d7093dd463ec4cd7
hana <ansgksk97@naver.com>
2024-01-17 12:48:35 +0900

    01/17 exception handler 추가

---
+++ src/main/java/com/green/gogiro/exception/AuthErrorCode.java
@@ -0,0 +1,23 @@
+package com.green.gogiro.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+
+@Getter
+public enum AuthErrorCode implements ErrorCode{
+
+    VALID_EXIST_USER_ID(HttpStatus.NOT_FOUND,"아이디가 존재하지 않습니다."),
+    VALID_PASSWORD(HttpStatus.NOT_FOUND,"비밀번호를 확인해주세요."),
+    NEED_SIGNIN(HttpStatus.UNAUTHORIZED,"로그인이 필요합니다."),
+    NOT_FOUND_REFRESH_TOKEN(HttpStatus.NOT_FOUND, "refresh-token이 없습니다.");
+
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    AuthErrorCode(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+}
