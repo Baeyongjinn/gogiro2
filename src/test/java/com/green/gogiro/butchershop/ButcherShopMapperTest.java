ed82ebe21e627519bf6179355a5ffdc7bbc7f79a
배용진 <dydwls2270@naver.com>
2024-01-23 17:00:07 +0900

    2024-01-23 게시판 5

--- src/test/java/com/green/gogiro/butchershop/ButcherShopMapperTest.java
+++ src/test/java/com/green/gogiro/butchershop/ButcherShopMapperTest.java
@@ -119,8 +119,8 @@ class ButcherShopMapperTest {
        // pics.add("https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20220116_288%2F1642311643318kwPLB_JPEG%2F85725C4B-61E2-4812-84F4-2787055384F3.jpeg");
        // pics.add("https://search.pstatic.net/common/?src=https%3A%2F%2Fpup-review-phinf.pstatic.net%2FMjAyMzExMDZfMjA2%2FMDAxNjk5MjY1ODg2OTMx.SzaZp3I0Jnoi2aQo27jxjJ7YHRL0_BZrmq3LGVN_mRMg.r8eb55qSiFzhxtmeHzUq6ryWqUlk6v8Jx_Z1_8xIUhog.JPEG%2F20231106_191705.jpg");
        // pics.add("https://search.pstatic.net/common/?src=https%3A%2F%2Fpup-review-phinf.pstatic.net%2FMjAyMzEwMTJfMiAg%2FMDAxNjk3MDcxMzk5OTA1.8dI82VAahXt7C-bJ-GkZj86jP3rYwPWuTpOnu40gLD0g.Nwsww0Qs13mJIvMLvRvWi_lSSsyrHcw1RY4Xhxfwmtog.JPEG%2F20231011_164501.jpg");
-      //  pics.add("https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20231002_284%2F16962130346305vjqz_JPEG%2F1000003022.jpg");
-      //  pics.add("https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20200215_156%2F1581763779726ttzb6_JPEG%2Fw2PQLx9eCUl-gV6J8A0-utOU.jpg");
+       // pics.add("https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20231002_284%2F16962130346305vjqz_JPEG%2F1000003022.jpg");
+       // pics.add("https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20200215_156%2F1581763779726ttzb6_JPEG%2Fw2PQLx9eCUl-gV6J8A0-utOU.jpg");
         ButcherSelVo vo = new ButcherSelVo();
         vo.setIbutcher(2);
         vo.setName("남문한우백화점 부림축산");
@@ -160,35 +160,48 @@ class ButcherShopMapperTest {
 
     @Test
     void selButcherShopDetail() throws  Exception{
-
+    ButcherShopDetailVo vo = new ButcherShopDetailVo();
+    vo.setIbutcher(1);
+    vo.setName("다정축산물직판장");
+    vo.setLocation("대구 중구 중앙대로 314");
+    vo.setOpen("매일\r\n08:30 - 22:30");
+    vo.setTel("0507-1393-9410");
+    vo.setX("126.726385532141");
+    vo.setY("35.1804742090837");
+    vo.setStar(5);
+    vo.setIsBook(0);
+    vo.setCount(0);
+    ButDto dto = new ButDto(0,1);
+    ButcherShopDetailVo vo2 = mapper.selButcherShopDetail(dto);
+    assertEquals(vo,vo2);
     }
 
     @Test
-    void selMenuDetail() throws  Exception{
+    void selMenuDetail() throws  Exception {
     }
 
 
     @Test
-    void selReviewDetail() throws  Exception{
+    void selReviewDetail() throws  Exception {
     }
 
 
     @Test
-    void selReviewPicDetail() throws  Exception{
+    void selReviewPicDetail() throws  Exception {
     }
 
 
 
     @Test
-    void selButcherPicsForTest() throws  Exception{
+    void selButcherPicsForTest() throws  Exception {
     }
 
     @Test
-    void selButcherReviewForTest() throws  Exception{
+    void selButcherReviewForTest() throws  Exception {
     }
 
     @Test
-    void selButcherBookmark() throws  Exception{
+    void selButcherBookmark() throws  Exception {
     }
 
 
