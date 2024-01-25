5945b214ab5a8a3b7894cefa2eb223eec6046e0d
배용진 <dydwls2270@naver.com>
2024-01-25 10:15:01 +0900

    2024-01-25 어드민 schema

--- src/main/java/com/green/gogiro/admin/model/ButcherPicsUpdDto.java
+++ src/main/java/com/green/gogiro/admin/model/ButcherPicsUpdDto.java
@@ -1,6 +1,7 @@
 package com.green.gogiro.admin.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,6 +11,7 @@ import java.util.List;
 @Data
 public class ButcherPicsUpdDto {
     private int ibutcher;
+    @Schema(example = "[]")
     private List<Integer> ibutPics;
     @JsonIgnore
     private List<String> pics = new ArrayList<>();
